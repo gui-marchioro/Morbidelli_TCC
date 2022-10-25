865c711667a174d6bc3eb9823dca27e6eb595aaf
gui-marchioro <gui02_marchioro@hotmail.com>
2022-10-25 00:07:13 -0300

    CHANGED: Partial implentation of Tool Changer program. It was added the start of the routine for the milling tool exchange.
    TO DO: Use the tool sensor to detect if there is a tool and finish the routine.

--- M6_ToolChanger.c
+++ M6_ToolChanger.c
@@ -1,3 +1,5 @@
+#include "Drill.c"
+#include "Spindle.c"
 #include "KMotionDef.h"
 
 #ifndef TMP
@@ -6,55 +8,40 @@
 #endif
 
 #define TOOL_VAR 9
+
+#define PREVIOUS_TOOL_VAR 191
+
 #define MILL_TOOL_SLOT_MIN 1
 #define MILL_TOOL_SLOT_MAX 10
-#define DRILL_BOX_SLOT_MIN 11
-#define DRILL_BOX_SLOT_MAX 31
+#define TOOL_PRESENT_INPUT 1049
+#define CLOSE_MAGAZINE_OUTPUT 1061
+#define MAGAZINE_CLOSED_INPUT 1040
+#define OPEN_MAGAZINE_OUTPUT 1062
+#define MAGAZINE_OPENED_INPUT 1041
+#define PISTON_TOOL_OUTPUT 1068
+#define SPHERE_GRIPPER_OUTPUT 1065
+#define OPEN_TOOL_GRIPPER_OUTPUT 1067
+#define PANTOGRAPH_OFF_SENSOR_INPUT 1042 //Pantografo recuado/alto
+#define PANTOGRAPH_MOVING_SENSOR_INPUT 1043 //Pantografo baixo
+#define PANTOGRAPH_ON_SENSOR_INPUT 1044 //Pantografo baixo
 
-#define DRILL_1_SLOT 11
-#define DRILL_2_SLOT 12
-#define DRILL_3_SLOT 13
-#define DRILL_4_SLOT 14
-#define DRILL_5_SLOT 15
-#define DRILL_6_SLOT 16
-#define DRILL_7_SLOT 17
-#define DRILL_8_SLOT 18
-#define DRILL_9_SLOT 19
-#define DRILL_10_SLOT 20
-#define DRILL_11_SLOT 21
-#define DRILL_12_SLOT 22
-#define DRILL_13_SLOT 23
-#define DRILL_14_SLOT 24
-#define DRILL_15_SLOT 25
-#define DRILL_16_SLOT 26
-#define DRILL_17_SLOT 27
-#define DRILL_18_SLOT 28
-#define DRILL_19_SLOT 29
-#define DRILL_20_SLOT 30
-#define DRILL_21_SLOT 31
 
-#define DRILL_MOTOR_OUTPUT 1069
-#define DRILL_1_PISTON_OUTPUT 48
-#define DRILL_2_PISTON_OUTPUT 49
-#define DRILL_3_PISTON_OUTPUT 50
-#define DRILL_4_PISTON_OUTPUT 51
-#define DRILL_5_PISTON_OUTPUT 52
-#define DRILL_6_PISTON_OUTPUT 53
-#define DRILL_7_PISTON_OUTPUT 54
-#define DRILL_8_PISTON_OUTPUT 55
-#define DRILL_9_PISTON_OUTPUT 56
-#define DRILL_10_PISTON_OUTPUT 57
-/*#define DRILL_11_PISTON_OUTPUT 
-#define DRILL_12_PISTON_OUTPUT 
-#define DRILL_13_PISTON_OUTPUT 
-#define DRILL_14_PISTON_OUTPUT 
-#define DRILL_15_PISTON_OUTPUT 
-#define DRILL_16_PISTON_OUTPUT 
-#define DRILL_17_PISTON_OUTPUT 
-#define DRILL_18_PISTON_OUTPUT 
-#define DRILL_19_PISTON_OUTPUT 
-#define DRILL_20_PISTON_OUTPUT 
-#define DRILL_21_PISTON_OUTPUT */
+// Each axis have 2000 counts per revolution
+// Resolution of X axis = 20 mm per revolution
+// Resolution of Y axis = 20 mm per revolution
+// Resolution of Z axis = 7.5 mm per revolution
+// FACTOR_X = 20 /2000 = 0.01000 mm/counts
+// FACTOR_Y = 20 /2000 = 0.01000 mm/counts
+// FACTOR_Z = 7.5/2000 = 0.00375 mm/counts
+#define FACTOR_X 0.01000
+#define FACTOR_Y 0.01000
+#define FACTOR_Z 0.00375
+#define X_AXIS 0
+#define Y_AXIS 1
+#define Z_AXIS 2
+#define OFFSET_X 20
+#define OFFSET_Y -20
+#define OFFSET_Z -7.5
 
 // Routine to be executed when M6 TX is called in a G program. (X is the tool number)
 void main ()
@@ -62,6 +49,11 @@ void main ()
     int slot = persist.UserData[TOOL_VAR];  // value stored is an int 
 	int id = persist.UserData[TOOL_VAR+1];  // value stored is an int 
 	printf("Tool Set to slot %d id %d\n",slot,id);  // print the slot and id
+
+    if (GetSpindleStatus != SPINDLE_OFF)
+    {
+        MDI("M5");
+    }
 
     ClearDrillOutputs();
 
@@ -80,132 +72,50 @@ void main ()
     }
 }
 
-// Clear all outputs related to the drill operation
-void ClearDrillOutputs()
-{
-    ClearBit(DRILL_MOTOR_OUTPUT);
-    ClearBit(DRILL_1_PISTON_OUTPUT);
-    ClearBit(DRILL_2_PISTON_OUTPUT);
-    ClearBit(DRILL_3_PISTON_OUTPUT);
-    ClearBit(DRILL_4_PISTON_OUTPUT);
-    ClearBit(DRILL_5_PISTON_OUTPUT);
-    ClearBit(DRILL_6_PISTON_OUTPUT);
-    ClearBit(DRILL_7_PISTON_OUTPUT);
-    ClearBit(DRILL_8_PISTON_OUTPUT);
-    ClearBit(DRILL_9_PISTON_OUTPUT);
-    ClearBit(DRILL_10_PISTON_OUTPUT);
-    /*ClearBit(DRILL_11_PISTON_OUTPUT);
-    ClearBit(DRILL_12_PISTON_OUTPUT);
-    ClearBit(DRILL_13_PISTON_OUTPUT);
-    ClearBit(DRILL_14_PISTON_OUTPUT);
-    ClearBit(DRILL_15_PISTON_OUTPUT);
-    ClearBit(DRILL_16_PISTON_OUTPUT);
-    ClearBit(DRILL_17_PISTON_OUTPUT);
-    ClearBit(DRILL_18_PISTON_OUTPUT);
-    ClearBit(DRILL_19_PISTON_OUTPUT);
-    ClearBit(DRILL_20_PISTON_OUTPUT);
-    ClearBit(DRILL_21_PISTON_OUTPUT);*/
-}
-
-// Select and detach the selected drill
-void DrillRoutine(int drillSlot)
-{
-    Delay_sec(0.25);
-    SetBit(DRILL_MOTOR_OUTPUT);
-    Delay_sec(0.25);
-    switch (drillSlot)
-    {
-        case DRILL_1_SLOT:
-                SetBit(DRILL_1_PISTON_OUTPUT);
-            break;
-
-        case DRILL_2_SLOT:
-                SetBit(DRILL_2_PISTON_OUTPUT);
-            break;
-        
-        case DRILL_3_SLOT:
-                SetBit(DRILL_3_PISTON_OUTPUT);
-            break;
-
-        case DRILL_4_SLOT:
-                SetBit(DRILL_4_PISTON_OUTPUT);
-            break;
-
-        case DRILL_5_SLOT:
-                SetBit(DRILL_5_PISTON_OUTPUT);
-            break;
-
-        case DRILL_6_SLOT:
-                SetBit(DRILL_6_PISTON_OUTPUT);
-            break;
-
-        case DRILL_7_SLOT:
-                SetBit(DRILL_7_PISTON_OUTPUT);
-            break;
-
-        case DRILL_8_SLOT:
-                SetBit(DRILL_8_PISTON_OUTPUT);
-            break;
-
-        case DRILL_9_SLOT:
-                SetBit(DRILL_9_PISTON_OUTPUT);
-            break;
-
-        case DRILL_10_SLOT:
-                SetBit(DRILL_10_PISTON_OUTPUT);
-            break;
-
-        /*case DRILL_11_SLOT:
-                SetBit(DRILL_11_PISTON_OUTPUT);
-            break;
-
-        case DRILL_12_SLOT:
-                SetBit(DRILL_12_PISTON_OUTPUT);
-            break;
-        
-        case DRILL_13_SLOT:
-                SetBit(DRILL_13_PISTON_OUTPUT);
-            break;
-
-        case DRILL_14_SLOT:
-                SetBit(DRILL_14_PISTON_OUTPUT);
-            break;
-
-        case DRILL_15_SLOT:
-                SetBit(DRILL_15_PISTON_OUTPUT);
-            break;
-
-        case DRILL_16_SLOT:
-                SetBit(DRILL_16_PISTON_OUTPUT);
-            break;
-
-        case DRILL_17_SLOT:
-                SetBit(DRILL_17_PISTON_OUTPUT);
-            break;
-
-        case DRILL_18_SLOT:
-                SetBit(DRILL_18_PISTON_OUTPUT);
-            break;
-
-        case DRILL_19_SLOT:
-                SetBit(DRILL_19_PISTON_OUTPUT);
-            break;
-
-        case DRILL_20_SLOT:
-                SetBit(DRILL_20_PISTON_OUTPUT);
-            break;
-
-        case DRILL_21_SLOT:
-                SetBit(DRILL_21_PISTON_OUTPUT);
-            break;*/
-
-        default:
-            break;
-    }
-}
-
 // Exchange the milling tool
 void MillExchangeRoutine(int millSlot)
 {
+    double xOffset = 0;
+    double yOffset = 0;
 
+    if (millSlot%2 == 0)
+    {
+        xOffset = 120;
+    }
+
+    if (millSlot > 6)
+    {
+        yOffset = 160*3;
+    }
+    else if (millSlot > 4)
+    {
+        yOffset = 160*2;
+    }
+    else if (millSlot > 2)
+    {
+        yOffset = 160*1;
+    }
+
+    double xPosition = (double) (23.6 + OFFSET_X + xOffset)/FACTOR_X;
+    double yPosition = (double) (-876.2 + OFFSET_Y + yOffset)/FACTOR_X;
+    double zPosition = (double) (3.15 + OFFSET_Z)/FACTOR_Z;
+    SetBit(OPEN_MAGAZINE_OUTPUT);
+    Move(X_AXIS, xPosition);
+    Move(Y_AXIS, yPosition);
+    while (!(CheckDone(X_AXIS) && CheckDone(Y_AXIS)));
+    Move(Z_AXIS, zPosition);
+    while (!CheckDone(Z_AXIS));
+    SetBit(PISTONS_ENABLE_OUTPUT);
+    SetBit(PISTONL_ENABLE_OUTPUT);
+    SetBit(PISTON_TOOL_OUTPUT);
+}
+
+void SetPreviousTool(int millSlot)
+{
+    persist.UserData[PREVIOUS_TOOL_VAR] = millSlot;
+}
+
+int GetPreviousTool()
+{
+    return persist.UserData[PREVIOUS_TOOL_VAR];
 }
