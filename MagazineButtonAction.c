1dd93e9cddf8b8c91e572497e128256bbbce26d5
gui-marchioro <gui02_marchioro@hotmail.com>
2022-11-09 10:38:58 -0300

    ADDED: Button action to open and close the tool magazine.

---
+++ MagazineButtonAction.c
@@ -0,0 +1,28 @@
+#include "MillChanger.c"
+#include "InitConfig.c"
+#include "Homing.c"
+#include "KMotionDef.h"
+
+#ifndef TMP
+#define TMP 10 // which spare persist to use to transfer data
+#include "KflopToKMotionCNCFunctions.c"
+#endif
+
+main()
+{
+    // Conditions to not execute command
+    if (GetInitExecuted() == 0 || GetHomingExecuted() == 0 || GetIsExecutingHoming() == 1 || JOB_ACTIVE)
+    {
+        MsgBoxNoWait("Execute the initialization and homing before execute this command.", MB_ICONASTERISK);
+        return;
+    }
+
+    if (ReadBit(MAGAZINE_CLOSED_INPUT))
+    {
+        OpenMagazine();
+    }
+    else
+    {
+        CloseMagazine();
+    }
+}
